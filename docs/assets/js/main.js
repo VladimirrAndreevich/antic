ecaedb321b083d3d9c1f1097a2baa850b4f2f34e
Vladimir <ryzhovvladimirr@gmail.com>
2023-01-20 18:37:22 +0200

    add header

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,7 +1,14 @@
-console.log(1)
-console.log(1)
-console.log(1)
-console.log(1)
-console.log(1)
+const btn = document.getElementById("burger");
+const nav = document.getElementById("menu");
+
+const close_menu = document.getElementById("close-menu");
+
+btn.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+});
+
+close_menu.addEventListener("click", () => {
+  nav.classList.toggle("hidden");
+});
 console.log(2)
 console.log(3)
