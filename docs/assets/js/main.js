6db4541d66f5553d283c310a139e9753a8ad90af
Vladimir <ryzhovvladimirr@gmail.com>
2023-01-31 16:44:43 +0200

    add section rooms with adaptive

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -10,5 +10,33 @@ btn.addEventListener("click", () => {
 close_menu.addEventListener("click", () => {
   nav.classList.toggle("hidden");
 });
-console.log(2)
+var swiper = new Swiper(".rooms__swiper", {
+  // Optional parameters
+  slidesPerView: "auto",
+  grabCursor: true,
+  centeredSlides: true,
+
+  pagination: {
+    el: ".rooms__swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".rooms__btn-next",
+  },
+  breakpoints: {
+    // when window width is >= 426px
+    426: {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+    500: {
+      slidesPerView: 1.5,
+    },
+    600: {
+      slidesPerView: 2,
+      centeredSlides: false,
+    },
+  },
+});
 console.log(3)
